692780022f8e4ab17fd9e21361721a549abd0ea3
RanjanKhadka012 <112820130+RanjanKhadka012@users.noreply.github.com>
2022-11-15 19:03:01 +0545

    1st one

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 console.log("Hello world 123s ");
 //alert("meta");
 document.write("this is a document")
+let a= document.getElementsByClassName('container');
+a[0].style.background = "yellow"
 
+function clicked(){
+    
+    document.write("hero kto moh")
+    ab=()=>{
+        console.log("oi samane")
+    }
+    setTimeout(ab,2000);
+}
+window.onload = function(){
+    console.log("the document has loaded")
+}
