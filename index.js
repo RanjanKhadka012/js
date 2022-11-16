5aa5448207ba54b035f6bf95ba8e63ee821addd3
RanjanKhadka012 <112820130+RanjanKhadka012@users.noreply.github.com>
2022-11-16 07:53:38 +0545

    final changes

--- index.js
+++
@@ -1,6 +0,0 @@
-a= new Date(); 
-        a.getHours()
-        let time=a.getHours()+":"+a.getMinutes()+":"+a.getSeconds();
-        let date = a.toLocaleDateString();
-
-document.getElementById('time').innerHTML = date + time;
